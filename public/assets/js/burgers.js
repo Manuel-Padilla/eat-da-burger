67332d4d1ef9121fec28274433ff3e92fcc00f3d
Manuel <manuelpadilla83@hotmail.com>
2019-01-02 02:43:05 -0600

    modified burgers.js

---
+++ public/assets/js/burgers.js
@@ -0,0 +1,57 @@
+$(function() {
+  $(".change-devour").on("click", function(event) {
+    var id = $(this).data("id");
+    var newDevour = $(this).data("newdevour");
+
+    var newDevourState = {
+      devoured: newDevour
+    };
+
+    // send PUT request
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newDevourState
+    }).then(function() {
+      console.log("changed devoured to", newDevour);
+      // reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".create-form").on("submit", function(event) {
+    // preventDefault on a submit event
+    event.preventDefault();
+
+    var newBurger = {
+      burger_name: $("#burger")
+        .val()
+        .trim(),
+      devoured: $("[name=devoured]:checked")
+        .val()
+        .trim()
+    };
+
+    // send POST request
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
+    }).then(function() {
+      console.log("created new burger");
+      // reload the page to get the updated list
+      location.reload();
+    });
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+
+    // send the DELETE request
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(function() {
+      console.log("deleted burger", id);
+      // reload the page to get the update list
+      location.reload();
+    });
+  });
+});
