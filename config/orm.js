97f9ceca35461a923d7747b737bdee491818f752
Manuel <manuelpadilla83@hotmail.com>
2019-01-02 02:12:00 -0600

    modified orm.js

---
+++ config/orm.js
@@ -0,0 +1,92 @@
+// Import MySQL connection
+const connection = require("../config/connection.js");
+
+// loops through and creates an array of question marks
+// ["?", "?", "?"] - and turns it into a string
+// ["?", "?", "?"].toString() => "?,?,?"
+function printQuestionMarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+// conver object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Burger => "Burger")
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      // e.g. {burger_name: 'Burger'} => ["burger_name='Burger'"]
+      // e.g. {devoured: true} => ["devoured=true"]
+      arr.push(key + " = " + value);
+    }
+  }
+
+  // translate array of strings to single comma-separated string
+  return arr.toString();
+}
+
+// Object for SQL statement functions
+const orm = {
+  selectAll: function(tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  insertOne: function(table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  // example objColVals {burger_name: Burger, devoured: true}
+  updateOne: function(table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  },
+  delete: function(table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function(err, result) {
+      if (err) throw err;
+      cb(result);
+    });
+  }
+};
+
+// export orm object for model (burger.js)
+module.exports = orm;
